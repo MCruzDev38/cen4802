87e9b13d2893252b427e86a2ce7213e8783741be
Michelle Cruz <172466227+MCruzDev38@users.noreply.github.com>
2026-08-28 23:03:51 -0400

    Add date added field to plant records

--- src/main/java/com/michelle/Plant.java
+++ src/main/java/com/michelle/Plant.java
@@ -1,51 +1,60 @@
 package com.michelle;
+
 public class Plant {
-        private int id;
-        private String name;
-        private String type;
-        private String wateringFrequency;
-        private String sunlight;
-        private String notes;
+    private int id;
+    private String name;
+    private String type;
+    private String wateringFrequency;
+    private String sunlight;
+    private String notes;
+    private String dateAdded;
 
-        public Plant(int id, String name, String type, String wateringFrequency, String sunlight, String notes) {
-            this.id = id;
-            this.name = name;
-            this.type = type;
-            this.wateringFrequency = wateringFrequency;
-            this.sunlight = sunlight;
-            this.notes = notes;
-        }
-
-        public Plant(String name, String type, String wateringFrequency, String sunlight, String notes) {
-            this.name = name;
-            this.type = type;
-            this.wateringFrequency = wateringFrequency;
-            this.sunlight = sunlight;
-            this.notes = notes;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public String getWateringFrequency() {
-            return wateringFrequency;
-        }
-
-        public String getSunlight() {
-            return sunlight;
-        }
-
-        public String getNotes() {
-            return notes;
-        }
+    public Plant(int id, String name, String type, String wateringFrequency,
+                 String sunlight, String notes, String dateAdded) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.wateringFrequency = wateringFrequency;
+        this.sunlight = sunlight;
+        this.notes = notes;
+        this.dateAdded = dateAdded;
     }
 
+    public Plant(String name, String type, String wateringFrequency,
+                 String sunlight, String notes, String dateAdded) {
+        this.name = name;
+        this.type = type;
+        this.wateringFrequency = wateringFrequency;
+        this.sunlight = sunlight;
+        this.notes = notes;
+        this.dateAdded = dateAdded;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getWateringFrequency() {
+        return wateringFrequency;
+    }
+
+    public String getSunlight() {
+        return sunlight;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+}
